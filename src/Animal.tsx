b465cc647393213dff74b879d4a81182b5df44a7
isaac-kapela <kapelajoao4@gmail.com>
2024-12-02 20:53:00 -0300

    add mais animais

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,18 +1,24 @@
 import "./Animal.css";
 
-interface AnimalProps{
-    icone:string;
+interface AnimalProps {
+    icone: string;
     nome: string;
     peso: number;
     extincao: boolean;
 }
 
-export default function ({icone, nome, peso, extincao}: AnimalProps) {
+export default function ({ icone, nome, peso, extincao }: AnimalProps) {
     return <div className="animal">
-    <div className="icone">{icone}</div>
-    <div className="nome">{nome}</div>
-    <div className="peso">{peso.toLocaleString()}</div>
-    <div className="extincao">{extincao ? "true" : "false"}</div>
+        <div className="icone">{icone}</div>
+        <div className="nome">{nome}</div>
+        {peso > 0.0 ?
+
+            <div className="peso">{peso.toLocaleString()}</div>
+            :
+            <div className="peso">Desconhecido</div>
+        }
+
+        <div className="extincao">{extincao ? "em extinção" : "não esta em extinção"}</div>
 
 
     </div>;
--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
       <div className='app'>
 
         <Animal icone="🦁" nome ="leao" peso={140} extincao={true} ></Animal>
+        <Animal icone='🦒' nome= "girafa" peso={400} extincao={true}></Animal>
+        <Animal icone="🐻" nome="urso" peso={200} extincao={true}></Animal>
 
       </div>
     </>
