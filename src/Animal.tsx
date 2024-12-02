37396ea762a8128f40b6aa46f3acb972a7346702
isaac-kapela <kapelajoao4@gmail.com>
2024-12-02 19:59:24 -0300

    add componente animal

---
+++ src/Animal.tsx
@@ -0,0 +1,8 @@
+import "./Animal.css";
+
+export default function () {
+    return <div className="animal">
+    animal
+
+    </div>;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Animal from './Animal'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>Ola mundo!</div>
+      <div className='app'>
+
+        <Animal></Animal>
+
+      </div>
     </>
   )
 }
